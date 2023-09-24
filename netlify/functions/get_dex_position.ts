1b3e3fed29d7a0b8f2531f03cb91dd02e6b91c0c
Max Koverdyaev <shadeglare@gmail.com>
2023-09-24 18:54:32 +0300

    setup

--- netlify/functions/get_dex_position.ts
+++ netlify/functions/get_dex_position.ts
@@ -1,8 +1,16 @@
 import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+import { PublicKey } from "@solana/web3.js";
 
-export const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+export const handler: Handler = async (event: HandlerEvent, _context: HandlerContext) => {
+  const input = JSON.parse(event.body!);
+  const poolAddress = new PublicKey(input.poolAddress);
+  const nftAddress = new PublicKey(input.nftAddress);
+
   return {
-    body: "Response here",
+    body: JSON.stringify({
+      poolAddress,
+      nftAddress
+    }),
     statusCode: 200
   };
 };
