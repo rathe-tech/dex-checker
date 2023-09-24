b73e60bb8ea5963832509a8bea7d00fbcbe6663c
Max Koverdyaev <shadeglare@gmail.com>
2023-09-24 19:20:51 +0300

    add dex method

--- netlify/functions/get_dex_position.ts
+++ netlify/functions/get_dex_position.ts
@@ -1,16 +1,19 @@
 import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
-import { PublicKey } from "@solana/web3.js";
+import { Connection, PublicKey } from "@solana/web3.js";
+import { getDexPosition } from "./shared/get_dex_position";
 
 export const handler: Handler = async (event: HandlerEvent, _context: HandlerContext) => {
   const input = JSON.parse(event.body!);
+
+  const dex = input.dex as any;
   const poolAddress = new PublicKey(input.poolAddress);
   const nftAddress = new PublicKey(input.nftAddress);
 
+  const connection = new Connection("https://api.mainnet-beta.solana.com");
+  const position = await getDexPosition({ connection, dex, poolAddress, nftAddress });
+
   return {
-    body: JSON.stringify({
-      poolAddress: poolAddress.toBase58(),
-      nftAddress: nftAddress.toBase58()
-    }),
+    body: JSON.stringify(position),
     statusCode: 200
   };
 };
---
+++ netlify/functions/shared/get_dex_position.ts
@@ -0,0 +1,41 @@
+import Decimal from "decimal.js"
+import {
+  Connection,
+  PublicKey
+} from "@solana/web3.js";
+import { getOrcaPosition } from "./get_orca_position";
+import { getRaydiumPosition } from "./get_raydium_position";
+
+export async function getDexPosition({
+  dex,
+  connection,
+  poolAddress,
+  nftAddress,
+}: {
+  dex: "RAYDIUM" | "ORCA",
+  connection: Connection,
+  poolAddress: PublicKey,
+  nftAddress: PublicKey,
+}): Promise<{
+  mints: {
+    address: PublicKey,
+    decimals: number,
+  }[],
+  liquidity: [Decimal, Decimal],
+  pendingFees: [Decimal, Decimal],
+  pendingRewards: Decimal[],
+  price: Decimal,
+  invertedPrice: Decimal,
+  range: [Decimal, Decimal],
+  invertedRange: [Decimal, Decimal],
+  inRange: boolean,
+}> {
+  switch (dex) {
+    case "ORCA":
+      return await getOrcaPosition({ connection, poolAddress, nftAddress });
+    case "RAYDIUM":
+      return await getRaydiumPosition({ connection, poolAddress, nftAddress });
+    default:
+      throw new Error(`Not supported DEX: ${dex}`);
+  }
+}
